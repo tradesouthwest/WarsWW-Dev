3e22582592f197e0f24b2c2377a735350c30e579
Larry <tradesouthwest@outlook.com>
2026-05-02 09:51:54 -0700

    Create SpecialReports custom post type

---
+++ cpts/special-reports-cpt.php
@@ -0,0 +1,25 @@
+<?php
+/* Editorial CPT
+ *
+ */
+
+namespace WarsWW\Editorial;
+
+class SpecialReports {
+    public static function register() {
+        register_post_type( 'ww_editorial', [
+            'labels' => [
+                'name'          => 'Special Reports',
+                'singular_name' => 'Special Report',
+                'add_new_item'  => 'Archive a New Intelligence Report',
+            ],
+            'public'       => true,
+            'has_archive'  => 'special-reports', // Slug: warsww.net/special-reports/
+            'show_in_rest' => true, // Enables Gutenberg
+            'menu_icon'    => 'dashicons-media-document',
+            'supports'     => [ 'title', 'editor', 'thumbnail', 'excerpt', 'custom-fields' ],
+            'rewrite'      => [ 'slug' => 'intel-report' ],
+        ]);
+    }
+}
+add_action( 'init', [ __NAMESPACE__ . '\SpecialReports', 'register' ] );
